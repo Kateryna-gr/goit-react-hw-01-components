1a08b7c33b55f025717a605f69171155cfd86a4e
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-10-18 23:37:36 +0300

    task 1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,20 @@
-export const App = () => {};
+import { Fragment } from 'react';
+import { Profile } from 'components/profile/profile';
+import data from './profile/user.json';
+import { Stats } from 'components/statistics/statistics';
+import statsData from './statistics/data.json';
+
+export const App = () => {
+  return (
+    <Fragment>
+      <Profile
+        username={data.username}
+        tag={data.tag}
+        location={data.location}
+        photo={data.avatar}
+        stats={data.stats}
+      />
+      <Stats title="Upload stats" statsData={statsData} />
+    </Fragment>
+  );
+};
